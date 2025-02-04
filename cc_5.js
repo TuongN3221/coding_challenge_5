fb4e795aad8f9024d92731c8724f9219a9882747
Tuong Nguyen <yolotube21@yahoo.com>
2025-02-04 15:33:21 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,11 @@
+// Task 1 - Object Properties
+let customer = {
+    name: "John Doe",
+    age: 35,
+    email: "johndoe@email.com"
+};// declares object with the properties
+
+// Logs each object as a template literal
+console.log(`Customer Name: ${customer.name}`);
+console.log(`Customer Age: ${customer.age}`);
+console.log(`Customer Email: ${customer.email}`);
